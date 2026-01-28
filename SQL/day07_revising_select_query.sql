31527e7ff83e725a649c906e73cf3ed21c19c61a
Rawan samir <rsmyr457@gmail.com>
2026-01-28 20:39:48 +0200

    Add SQL query for city and state selection
    Add SQL query to select city and state from station.

---
+++ SQL/day07_revising_select_query.sql
@@ -0,0 +1,9 @@
+-- Platform: HackerRank
+-- Language: SQL
+-- Problem: https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+--day:07
+-- Difficulty: Easy
+--solution 
+SELECT city , state
+from station ;
+
