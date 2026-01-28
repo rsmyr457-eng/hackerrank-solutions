3ddbbe421174ff3c31e4ca2384e26398919efd11
Rawan samir <rsmyr457@gmail.com>
2026-01-28 20:30:14 +0200

    Add SQL query for Japanese cities
    Add SQL query to select cities from Japan

---
+++ SQL/day05_revising_select_query.sql
@@ -0,0 +1,9 @@
+-- Platform: HackerRank
+-- Language: SQL
+-- Day: 05
+-- Problem: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+-- Difficulty: Easy
+--solution
+SELECT *
+from city 
+where countrycode ='JPN';
