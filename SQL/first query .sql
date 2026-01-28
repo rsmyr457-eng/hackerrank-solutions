8a9259f015c4db4fc2c55db24631eaa853e5b76e
Rawan samir <rsmyr457@gmail.com>
2026-01-28 19:54:08 +0200

    Create SQL query for American cities with large populations
    Add SQL query to retrieve cities in the USA with populations over 100,000.

---
+++ SQL/first query .sql
@@ -0,0 +1,8 @@
+#problem 
+Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+
+The CITY table is described as follows:
+
+#solution 
+SELECT * from city 
+where population >100000 and countrycode='USA' ;
