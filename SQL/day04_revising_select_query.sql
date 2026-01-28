ff96b54934a355707eca98bb28e09316097cef25
Rawan samir <rsmyr457@gmail.com>
2026-01-28 20:24:24 +0200

    Add query to select city by ID
    Add SQL query to select city by ID 1661

---
+++ SQL/day04_revising_select_query.sql
@@ -0,0 +1,9 @@
+-- Platform: HackerRank
+-- Language: SQL
+-- Day: 04
+-- Problem: https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+-- Difficulty: Easy
+-- solution 
+SELECT * 
+from city 
+where id=1661;
