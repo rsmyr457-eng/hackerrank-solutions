43d6f968b81b0685f2a14ee0e887f64596a61ebc
Rawan samir <rsmyr457@gmail.com>
2026-01-28 20:34:43 +0200

    Create day06_revising_select_query.sql

---
+++ SQL/day06_revising_select_query.sql
@@ -0,0 +1,9 @@
+-- Platform: HackerRank
+-- Language: SQL
+-- Day: 06
+-- Problem: https://www.hackerrank.com/challenges/japanese-cities-name/problem?isFullScreen=true
+-- Difficulty: Easy
+--solution 
+SELECT name 
+from city 
+where countrycode='JPN';
