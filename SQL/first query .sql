999a4ebe60ac7b190f43193e9a773e3cb50f3977
Rawan samir <rsmyr457@gmail.com>
2026-01-28 20:01:49 +0200

    Update SQL query for American cities with population

--- SQL/first query .sql
+++ SQL/first query .sql
@@ -1,8 +1,8 @@
-#problem 
-Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
-
-The CITY table is described as follows:
-
-#solution 
+-- Platform: HackerRank
+-- Language: SQL
+-- Day: 01
+-- Problem: https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+-- Difficulty: Easy
+--solution 
 SELECT * from city 
 where population >100000 and countrycode='USA' ;
