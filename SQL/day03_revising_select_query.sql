142a302e2c9c42b8536f68d261dc0e3d5a80c2e8
Rawan samir <rsmyr457@gmail.com>
2026-01-28 20:21:21 +0200

    Add select query for city table
    Add SQL query to select all records from the city table.

---
+++ SQL/day03_revising_select_query.sql
@@ -0,0 +1,8 @@
+-- Platform: HackerRank
+-- Language: SQL
+-- Day: 03
+-- Problem: https://www.hackerrank.com/challenges/select-all-sql/problem?isFullScreen=true
+-- Difficulty: Easy
+--solution 
+select *
+from city ;
