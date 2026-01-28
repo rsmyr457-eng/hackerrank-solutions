def029338abc0d5b6bbd4ec7ec060adc72dc4244
Rawan samir <rsmyr457@gmail.com>
2026-01-28 20:10:27 +0200

    Create second-day SQL solution for city population
    Add SQL solution for HackerRank Day 02 challenge.

---
+++ SQL/second -day .sql
@@ -0,0 +1,8 @@
+-- Platform: HackerRank
+-- Language: SQL
+-- Day: 02
+-- Problem: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+-- Difficulty: Easy
+--solution 
+SELECT name from city 
+where population>120000 and countrycode='USA';
