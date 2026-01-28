2a973ea972f2dc55cefc26ce3aa78b0ed9d6caec
Rawan samir <rsmyr457@gmail.com>
2026-01-28 20:16:52 +0200

    Format SQL query for better readability

--- SQL/day01_revising_select_query.sql
+++ SQL/day01_revising_select_query.sql
@@ -4,5 +4,7 @@
 -- Problem: https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
 -- Difficulty: Easy
 --solution 
-SELECT * from city 
-where population >100000 and countrycode='USA' ;
+SELECT * 
+from city 
+where population >100000
+  and countrycode='USA' ;
