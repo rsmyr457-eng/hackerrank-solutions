6caaa3d9e7e5f59cd2564e0d11c18d853ca11826
Rawan samir <rsmyr457@gmail.com>
2026-01-28 20:17:46 +0200

    Improve formatting of select query in SQL file
    Reformatted SQL query for better readability.

--- SQL/day02_revising_select_query.sql
+++ SQL/day02_revising_select_query.sql
@@ -4,5 +4,7 @@
 -- Problem: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
 -- Difficulty: Easy
 --solution 
-SELECT name from city 
-where population>120000 and countrycode='USA';
+SELECT name
+  from city 
+where population>120000 
+  and countrycode='USA';
