e1e577d058e3e09f27dada4e78340a7366dbc696
Rawan samir <rsmyr457@gmail.com>
2026-02-10 21:20:29 +0200

    Add solution for HackerRank Python loops problem

---
+++ python/day03.py
@@ -0,0 +1,9 @@
+# Platform: HackerRank
+# Language: Python
+# Problem: https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+# Day: 03
+# Difficulty: introduction
+# Solution
+n=int(input())
+for i in range(n):
+    print(i**2)
