2bb887dd79aa289657213a6ad12c46c2872c537c
Rawan samir <rsmyr457@gmail.com>
2026-02-10 21:11:24 +0200

    Add solution for HackerRank Python division problem
    Implement division solution for HackerRank Day 02 challenge.

---
+++ python/day02.py
@@ -0,0 +1,10 @@
+# Platform: HackerRank
+# Language: Python
+# Problem: https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+# Day: 02
+# Difficulty: introduction
+# Solution
+a=int(input())
+b=int(input())
+print(a//b)
+print(a/b)
