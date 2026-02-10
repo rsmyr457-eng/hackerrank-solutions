034081052e52e834dbc292194c512549ac2abfe3
Rawan samir <rsmyr457@gmail.com>
2026-02-10 21:07:30 +0200

    Add arithmetic operations in day0.py
    Implement basic arithmetic operations for two integers.

---
+++ python/day0.py
@@ -0,0 +1,11 @@
+# Platform: HackerRank
+# Language: Python
+# Problem: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+# Day: 00
+# Difficulty: introduction
+# Solution
+a=int(input())
+b=int(input())
+print(a+b)
+print(a-b)
+print(a*b)
