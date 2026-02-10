ec723f883cde6608b4946a2737cc0429d26b2854
Rawan samir <rsmyr457@gmail.com>
2026-02-10 21:51:39 +0200

    Add solution for Python print problem
    Implement solution for HackerRank Day 05 challenge.

---
+++ python/day05.py
@@ -0,0 +1,9 @@
+# Platform: HackerRank
+# Language: Python
+# Problem: https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+# Day: 05
+# Difficulty: introduction
+# Solution
+n=int(input())
+for i in range(1,n+1):
+    print(str(i) ,end="")
