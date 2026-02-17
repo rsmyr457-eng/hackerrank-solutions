6a7e5849a2688d2aa7d2ef8f4b7768faaba743ab
Rawan samir <rsmyr457@gmail.com>
2026-02-17 23:51:50 +0200

    Create SQL query for employee name selection
    Add SQL query to select and order employee names

---
+++ SQL/day17_revising_select_query.sql
@@ -0,0 +1,8 @@
+-- Platform: HackerRank
+-- Language: SQL
+-- Day: 17
+-- Problem: https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Difficulty: Easy
+--solution 
+SELECT name from employee
+order by name
