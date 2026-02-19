b603d2884933073a316827666310371a568380d5
Rawan samir <rsmyr457@gmail.com>
2026-02-19 21:06:28 +0200

    Add SQL query to filter cities by vowel criteria
    Added a SQL query to select distinct cities not starting or ending with vowels.

---
+++ SQL/day18_revising_select_query.sql
@@ -0,0 +1,10 @@
+-- Platform: HackerRank
+-- Language: SQL
+-- Day: 18
+-- Problem: https://www.hackerrank.com/challenges/weather-observation-station-11/problem?isFullScreen=true
+-- Difficulty: Easy
+--solution 
+select distinct city 
+from station 
+where city not REGEXP '^[aeoiu]'
+or city not REGEXP '[aeoiu]$'
