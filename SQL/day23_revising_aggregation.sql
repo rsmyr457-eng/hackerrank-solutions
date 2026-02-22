26586cf0948e987c42e5024b404f38a94e7a0ab6
Rawan samir <rsmyr457@gmail.com>
2026-02-22 15:31:01 +0200

    Add SQL query to count cities with population > 100000
    Add SQL solution for revising aggregation challenge.

---
+++ SQL/day23_revising_aggregation.sql
@@ -0,0 +1,8 @@
+-- Platform: HackerRank
+-- Language: SQL
+-- Day: 23
+-- Problem: https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=true
+-- Difficulty: Easy
+--solution 
+SELECT count(*) from city 
+where  population>100000;
