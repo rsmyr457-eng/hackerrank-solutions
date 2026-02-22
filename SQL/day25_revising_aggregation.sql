c8091bd36b65bd2672a9fe288ca3cfa06a7ea8a8
Rawan samir <rsmyr457@gmail.com>
2026-02-22 15:41:13 +0200

    Create SQL script for average population query
    Add SQL solution for calculating average population in California.

---
+++ SQL/day25_revising_aggregation.sql
@@ -0,0 +1,9 @@
+-- Platform: HackerRank
+-- Language: SQL
+-- Day: 25
+-- Problem: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true
+-- Difficulty: Easy
+--solution 
+SELECT avg(population)
+from city 
+where district ='California';
