fad66623f72a2d2ef2c07c1636470ad6fc9380f7
Rawan samir <rsmyr457@gmail.com>
2026-02-22 15:43:53 +0200

    Add SQL solution for average population challenge
    Add SQL solution for calculating average population.

---
+++ SQL/day26_revising_aggregation.sql
@@ -0,0 +1,8 @@
+-- Platform: HackerRank
+-- Language: SQL
+-- Day: 26
+-- Problem: https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+-- Difficulty: Easy
+--solution 
+SELECT round(avg(population))
+from city ;
